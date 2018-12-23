145d9ac485e5c3adeb11a4ad8fac243ad7f6fe22
singcl <24661881@qq.com>
2018-12-24 01:21:13 +0800

    :art: Create a serve use Go framework Gin.

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"github.com/gin-contrib/static"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+	r.Use(static.Serve("/", static.LocalFile("./public", true)))
+
+	r.Run()
+}
