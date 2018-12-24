b0860f806fb123cbd65e1e3abfeb6a9b95dfcacf
singcl <24661881@qq.com>
2018-12-25 00:49:40 +0800

    :art: update the dep

--- main.go
+++ main.go
@@ -1,13 +1,29 @@
 package main
 
 import (
+	"runtime"
+	"strconv"
+
 	"github.com/gin-contrib/static"
 	"github.com/gin-gonic/gin"
+	"github.com/pbnjay/memory"
 )
 
 func main() {
 	r := gin.Default()
 	r.Use(static.Serve("/", static.LocalFile("./public", true)))
+
+	// 静态资源设置
+	r.Static("/css", "/public/css")
+	r.Static("/js", "/public/js")
+
+	r.GET("/api/specs", func(c *gin.Context) {
+		c.JSON(200, []string{
+			runtime.GOOS,
+			strconv.Itoa(runtime.NumCPU()),
+			strconv.FormatUint(memory.TotalMemory()/(1024*1024), 10),
+		})
+	})
 
 	r.Run()
 }
