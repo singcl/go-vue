dba3478d65c67230fc2e7cf296c091ae56abc849
singcl <1276837689@qq.com>
2019-01-05 15:26:16 +0800

    :art: 增加一个POST路由

--- routes/index.go
+++ routes/index.go
@@ -1,1 +1,38 @@
 package routes
+
+import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+	"github.com/singcl/go-vue/db"
+)
+
+type MemData struct {
+	Data []float64 `binding: "required"`
+}
+
+type DbSchema struct {
+	Id   uint
+	Data []float64 `binding:"required"`
+}
+
+func init() {
+	db.SetupDb()
+}
+
+func Persist(c *gin.Context) {
+	data := new(MemData)
+	c.Bind(&data)
+
+	fmt.Printf("Persist数为：%+v", data)
+
+	memDB := db.Database
+	txn := memDB.Txn(true)
+
+	p := &DbSchema{uint(1), data.Data}
+	if err := txn.Insert("data", p); err != nil {
+		panic(err)
+	}
+
+	txn.Commit()
+}
