850ac07b646f631792bb86e30ef15a0e0a9378e4
singcl <1276837689@qq.com>
2019-01-07 15:14:00 +0800

    :art: go-memdb 内存数据库简单注释

--- db/setup.go
+++ db/setup.go
@@ -5,20 +5,23 @@ import memdb "github.com/hashicorp/go-memdb"
 var Database *memdb.MemDB
 
 func SetupDb() {
+	// 创建数据库的 DBSchema , 它包括多个表结构：  Tables map[string]*TableSchema
 	schema := &memdb.DBSchema{
 		Tables: map[string]*memdb.TableSchema{
+			// 每个表结构是 TableSchema, 它定义对应的index
 			"data": &memdb.TableSchema{
 				Name: "data",
 				Indexes: map[string]*memdb.IndexSchema{
 					"id": &memdb.IndexSchema{
 						Name:    "id",
 						Unique:  true,
-						Indexer: &memdb.UintFieldIndex{Field: "Id"},
+						Indexer: &memdb.UintFieldIndex{Field: "Id"}, // 从代码来看，是基于 Id 来建立 index
 					},
 				},
 			},
 		},
 	}
 
+	// 基于schema 创建数据库
 	Database, _ = memdb.NewMemDB(schema)
 }
--- routes/index.go
+++ routes/index.go
@@ -16,6 +16,7 @@ type DbSchema struct {
 	Data []float64 `binding:"required"`
 }
 
+// init函数import的时候会执行 详情查看init() main() 相关函数
 func init() {
 	db.SetupDb()
 }
@@ -27,12 +28,27 @@ func Persist(c *gin.Context) {
 	fmt.Printf("Persist数为：%+v", data)
 
 	memDB := db.Database
-	txn := memDB.Txn(true)
 
+	// 创建写事务
+	txn := memDB.Txn(true)
 	p := &DbSchema{uint(1), data.Data}
+	// 插入记录
 	if err := txn.Insert("data", p); err != nil {
 		panic(err)
 	}
-
+	// Commit
 	txn.Commit()
 }
+
+// // 创建只读事务
+// txn = db.Txn(false)
+// defer txn.Abort()
+
+// // 返回第一个符合的记录
+// raw, err := txn.First("person", "id", "joe@aol.com")
+// if err != nil {
+//     panic(err)
+// }
+
+// // Say hi!
+// fmt.Printf("Hello %s!", raw.(*Person).Name)
